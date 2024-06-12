744b50ba5e782ab63c0de234aabe74d4b38d32d7
chujiyang <chujiayang1421@outlook.com>
2024-06-12 00:31:07 -0700

    test commit

--- src/main/java/frc/robot/subsystems/Flywheels.java
+++ src/main/java/frc/robot/subsystems/Flywheels.java
@@ -1,7 +1,24 @@
 package frc.robot.subsystems;
 
-public class Flywheels {
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-    public class 
+public class Flywheels extends SubsystemBase {
+
+    private final TalonFX topFlywheel;
+    private final TalonFX bottomFlywheel;
+
+    private static Flywheels instance;
+
+    public static Flywheels getInstance() {
+        if (instance == null) {
+            instance = new Flywheels();
+        }
+        return instance;
+    }
+
+    private Flywheels() {
+        
+    }
     
 }
